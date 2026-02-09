122f1403b67a5510bc252dfdadcde9d68443527a
manb <manikandan.b.ext@nokia.com>
2026-02-09 22:21:02 +0530

    day1 of learning

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,6 +1,15 @@
-const {test} = require("@playwright/test");
-test("Firstplaywright test", async ()=>
+const {test} = require('@playwright/test');
+
+
+test ('With browser context playwright test',async({browser})=>
 {
-    //this is the first command
-    //second command
-})
+    //chrome 
+    const Context = await browser.newContext();
+    const page = await Context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+});
+
+test.only ("Without browser context playwright test2",async({page})=>
+{
+    await page.goto("https://google.com");
+});
